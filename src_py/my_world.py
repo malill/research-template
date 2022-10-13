7bae51b8d302f503db405ec4e8f0546427503440
malill <35307939+malill@users.noreply.github.com>
2022-10-13 12:14:34 +0200

    feat: use tf.keras

--- src_py/my_world.py
+++ src_py/my_world.py
@@ -1,10 +1,5 @@
 import tensorflow as tf
 import datetime as dt
-from keras import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.utils.np_utils import to_categorical
 
 
 def hello_world():
@@ -12,7 +7,7 @@ def hello_world():
 
 
 def tf_test(device):
-    with tf.device(f'/{device}:0'):
+    with tf.device(f"/{device}:0"):
         # print(f"Tensorflow version: {tf.__version__}")
         # print(f"Tensorflow.keras version: {tf.keras.__version__}")
 
@@ -26,22 +21,21 @@ def tf_test(device):
         (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
         # model
-        model = Sequential()
-        model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(28, 28, 1)))
-        model.add(MaxPooling2D((2, 2)))
-        # model.summary()
-
+        model = tf.keras.models.Sequential()
+        model.add(
+            tf.keras.layers.Conv2D(
+                32, (5, 5), activation="relu", input_shape=(28, 28, 1)
+            )
+        )
         # add new layers
-        model = Sequential()
-        model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(28, 28, 1)))
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (5, 5), activation='relu'))
-        model.add(MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+        model.add(tf.keras.layers.Conv2D(64, (5, 5), activation="relu"))
+        model.add(tf.keras.layers.MaxPooling2D((2, 2)))
         # model.summary()
 
         # classifier layer
-        model.add(Flatten())
-        model.add(Dense(10, activation='softmax'))
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(10, activation="softmax"))
         # model.summary()
 
         # print("Training images:\n{}".format(train_images.shape))
@@ -49,20 +43,22 @@ def tf_test(device):
 
         # reshape the data
         train_images = train_images.reshape((60000, 28, 28, 1))
-        train_images = train_images.astype('float32') / 255
+        train_images = train_images.astype("float32") / 255
 
         test_images = test_images.reshape((10000, 28, 28, 1))
-        test_images = test_images.astype('float32') / 255
+        test_images = test_images.astype("float32") / 255
 
-        train_labels = to_categorical(train_labels)
-        test_labels = to_categorical(test_labels)
+        train_labels = tf.keras.utils.to_categorical(train_labels)
+        test_labels = tf.keras.utils.to_categorical(test_labels)
 
         # print("\nAfter reshaping...")
         # print("Training images:\n{}".format(train_images.shape))
         # print("Training labels:\n{}".format(train_labels.shape))
 
         # compile the model
-        model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+        model.compile(
+            loss="categorical_crossentropy", optimizer="sgd", metrics=["accuracy"]
+        )
 
         # check time taken on the GPU for training and testing
 
@@ -75,5 +71,5 @@ def tf_test(device):
         test_loss, test_acc = model.evaluate(test_images, test_labels)
         end = dt.datetime.now()
 
-        print('Test accuracy: {}'.format(test_acc))
-        print(f'\nTime taken on the {device}: {end - start}')
+        print("Test accuracy: {}".format(test_acc))
+        print(f"\nTime taken on the {device}: {end - start}")
